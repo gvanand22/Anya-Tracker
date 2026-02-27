79cf42ed2536551bbd46fc64f9f9e80c7f6b424a
tempchck-stack <110307201+gvanand22@users.noreply.github.com>
2026-02-27 17:54:18 +0530

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Router>
+      <div className="navi">
+        <nav className="nav_Bar" id="nav-Bar">
+          <ul>
+            <li>AnRun</li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">Activity</Link></li>
+            <li><Link to="/">Analytics</Link></li>
+            <li><Link to="/">Goals</Link></li>
+            <li><Link to="/">Settings</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
+  )
 }
-
-export default App;
