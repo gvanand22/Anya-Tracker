7a2b8127e37fd940b8af53ce3f694dc0ad920494
tempchck-stack <110307201+gvanand22@users.noreply.github.com>
2026-03-02 11:56:20 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,47 +2,47 @@ function Home() {
     return(
         <div className="Home">
             <main>
-                <sec className="tab_Grp" id="tab-Grp">
-                    <tab className="tab_One" id="tab-One">
+                <div className="tab_Grp" id="tab-Grp">
+                    <section className="tab_One" id="tab-One">
                         <h2 className="dis_Tot" id="dis-Tot">
-                            <span></span> Kms
+                            <span>0</span> Kms
                         </h2>
                         <hr/>
                         <h4>Total Distance</h4>
-                    </tab>
-                    <tab className="tab_One" id="tab-One">
+                    </section>
+                    <section className="tab_Two" id="tab-Two">
                         <h2 className="tim_Tot" id="tim-Tot">
-                            <span></span> Mins
+                            <span>0</span> Mins
                         </h2>
                         <hr/>
                         <h4>Total Duration</h4>
-                    </tab>
-                    <tab className="tab_One" id="tab-One">
+                    </section>
+                    <section className="tab_Thr" id="tab-Thr">
                         <h2 className="cal_Tot" id="cal-Tot">
-                            <span></span> Kcal
+                            <span>0</span> Kcal
                         </h2>
                         <hr/>
                         <h4>Total Calories</h4>
-                    </tab>
-                    <tab className="tab_One" id="tab-One">
+                    </section>
+                    <section className="tab_For" id="tab-For">
                         <h2 className="stp_Tot" id="stp-Tot">
-                            <span></span> Steps
+                            <span>0</span> Steps
                         </h2>
                         <hr/>
                         <h4>Total Steps</h4>
-                    </tab>
-                    <tab className="tab_One" id="tab-One">
+                    </section>
+                    <section className="tab_Fiv" id="tab-Fiv">
                         <h2 className="dat_Tod" id="dat-Tod">Date</h2>
                         <h4 className="day_Tod" id="day-Tod">day</h4>
-                    </tab>
-                    <tab className="tab_One" id="tab-One">
+                    </section>
+                    <section className="tab_Six" id="tab-Six">
                         <h2 className="str_Tot" id="str-Tot">
-                            <span></span> Days
+                            <span>0</span> Days
                         </h2>
                         <hr/>
                         <h4>Active Streak</h4>
-                    </tab>
-                </sec>
+                    </section>
+                </div>
             </main>
         </div>
     )
