41034c61badd26a5634bf0dcb54e6552e1cbba5f
tempchck-stack <110307201+gvanand22@users.noreply.github.com>
2026-03-02 10:58:46 +0530

    Adding tab details to home

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,35 @@ function Home() {
         <div className="Home">
             <main>
                 <sec className="tab_Grp" id="tab-Grp">
-                    <tab className="tab_One" id="tab-One"></tab>
-                    <tab className="tab_One" id="tab-One"></tab>
-                    <tab className="tab_One" id="tab-One"></tab>
-                    <tab className="tab_One" id="tab-One"></tab>
-                    <tab className="tab_One" id="tab-One"></tab>
-                    <tab className="tab_One" id="tab-One"></tab>
+                    <tab className="tab_One" id="tab-One">
+                        <h2 className="dis_Tot" id="dis-Tot"></h2>
+                        <hr/>
+                        <h4>Total Distance</h4>
+                    </tab>
+                    <tab className="tab_One" id="tab-One">
+                        <h2 className="tim_Tot" id="tim-Tot"></h2>
+                        <hr/>
+                        <h4>Total Duration</h4>
+                    </tab>
+                    <tab className="tab_One" id="tab-One">
+                        <h2 className="cal_Tot" id="cal-Tot"></h2>
+                        <hr/>
+                        <h4>Total Calories</h4>
+                    </tab>
+                    <tab className="tab_One" id="tab-One">
+                        <h2 className="stp_Tot" id="stp-Tot"></h2>
+                        <hr/>
+                        <h4>Total Steps</h4>
+                    </tab>
+                    <tab className="tab_One" id="tab-One">
+                        <h2 className="dat_Tod" id="dat-Tod"></h2>
+                        <h4 className="day_Tod" id="day-Tod"></h4>
+                    </tab>
+                    <tab className="tab_One" id="tab-One">
+                        <h2 className="str_Tot" id="str-Tot"></h2>
+                        <hr/>
+                        <h4>Active Streak</h4>
+                    </tab>
                 </sec>
             </main>
         </div>
